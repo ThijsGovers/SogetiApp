a502c12c9f2b9d18866b59271068e2b94437c80e
ThijsGoofy <thijs.govers@student.nhlstenden.com>
2022-03-21 11:47:54 +0100

    KindOpgevenView

--- SogetiTG/Doneren/KindOpgevenView.swift
+++ SogetiTG/Doneren/KindOpgevenView.swift
@@ -12,62 +12,94 @@ struct KindOpgevenView: View {
     @State private var leeftijdKind = ""
     @State private var adres1 = ""
     @State private var adres2 = ""
+    @State private var hobbys = ""
+    @State private var verlanglijstje = ""
+    @State private var topCadeau = ""
     
     var body: some View {
             ZStack{
                 Color("LightGreen")
                 
-                VStack{
-                    Text("Naam van het kind")
-                        .font(.title2)
-                        .foregroundColor(Color("TextColor"))
-//                  TextView(text: "Voornaam en Achternaam")
-                    TextField("Voornaam en achternaam", text: $naamKind)
-                        .padding()
-                        .background(Color("ColorWhite"))
-                        .cornerRadius(20)
-                        
-                    
-                    Text("Leeftijd van het kind")
-                        .font(.title2)
-                        .foregroundColor(Color("TextColor"))
-                    TextField("Leeftijd", text: $leeftijdKind)
-                        .padding()
-                        .background(Color("ColorWhite"))
-                        .cornerRadius(20)
-                        .padding()
-                    
-                    Text("Geslacht")
-                        .font(.title2)
-                        .foregroundColor(Color("TextColor"))
-                    HStack{
-                        Image(systemName: "Circle.fill").foregroundColor(.white).font(.title2)
-                            .padding()
-                            .background(Color("ColorWhite"))
-                            .cornerRadius(20)
-                            .padding()
-                        Image(systemName: "Circle.fill").foregroundColor(.white).font(.title2)
-                            .padding()
-                            .background(Color("ColorWhite"))
-                            .cornerRadius(20)
-                            .padding()
+                ScrollView{
+                    VStack{
+                        VStack{
+                            Text("Naam van het kind")
+                                .font(.title2)
+                                .foregroundColor(Color("TextColor"))
+                                .padding(.top, 50)
+        //                  TextView(text: "Voornaam en Achternaam")
+                            TextField("Voornaam en achternaam", text: $naamKind)
+                                .padding()
+                                .background(Color("ColorWhite"))
+                                .cornerRadius(20)
+                                
+                            
+                            Text("Leeftijd van het kind")
+                                .font(.title2)
+                                .foregroundColor(Color("TextColor"))
+                            TextField("Leeftijd", text: $leeftijdKind)
+                                .padding()
+                                .background(Color("ColorWhite"))
+                                .cornerRadius(20)
+                            
+                            Text("Geslacht")
+                                .font(.title2)
+                                .foregroundColor(Color("TextColor"))
+                            HStack{
+                                Image(systemName: "Circle.fill").foregroundColor(.white).font(.title2)
+                                    .padding()
+                                    .background(Color("ColorWhite"))
+                                    .cornerRadius(20)
+                                    .padding()
+                                Image(systemName: "Circle.fill").foregroundColor(.white).font(.title2)
+                                    .padding()
+                                    .background(Color("ColorWhite"))
+                                    .cornerRadius(20)
+                                    .padding()
+                            }
+                            
+                            Text("Woonplaats van het kind")
+                                .font(.title2)
+                                .foregroundColor(Color("TextColor"))
+                            TextField("Adres", text: $adres1)
+                                .padding()
+                                .background(Color("ColorWhite"))
+                                .cornerRadius(20)
+                            TextField("Eventueel tweede adres", text: $adres2)
+                                .padding()
+                                .background(Color("ColorWhite"))
+                                .cornerRadius(20)
+                        }.padding()
                     }
                     
-                    Text("Woonplaats van het kind")
-                        .font(.title2)
-                        .foregroundColor(Color("TextColor"))
-                    TextField("Adres", text: $adres1)
-                        .padding()
-                        .background(Color("ColorWhite"))
-                        .cornerRadius(20)
-                        .padding()
-                    TextField("Eventueel tweede adres", text: $adres2)
-                        .padding()
-                        .background(Color("ColorWhite"))
-                        .cornerRadius(20)
-                        .padding()
-                }.padding()
-                
+                    VStack{
+                        Text("Hobby's van het kind")
+                            .font(.title2)
+                            .foregroundColor(Color("TextColor"))
+                        TextField("Hobby's", text: $hobbys)
+                            .padding()
+                            .background(Color("ColorWhite"))
+                            .cornerRadius(20)
+                        Text("Verlanglijstje van het kind")
+                            .font(.title2)
+                            .foregroundColor(Color("TextColor"))
+                        TextField("Verlanglijstje", text: $verlanglijstje)
+                            .padding()
+                            .background(Color("ColorWhite"))
+                            .cornerRadius(20)
+                        Text("Topcadeau")
+                            .font(.title2)
+                            .foregroundColor(Color("TextColor"))
+                        TextField("Top Cadeau", text: $topCadeau)
+                            .padding()
+                            .background(Color("ColorWhite"))
+                            .cornerRadius(20)
+                        
+                        VolgendeView(destination: BedanktView(), text: "Opgeven")
+                            .padding(.top, 30)
+                            .padding(.bottom, 30)
+                    }.padding()
+                }
             }.navigationBarTitle("Kind opgeven", displayMode: .inline)
     }
 }
