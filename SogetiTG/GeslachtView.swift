ca9844cb448d1700ef790e5c46a458d0d6467911
ThijsGoofy <thijs.govers@student.nhlstenden.com>
2022-03-29 11:28:06 +0200

    Gender buttons

--- SogetiTG/GeslachtView.swift
+++ SogetiTG/GeslachtView.swift
@@ -23,8 +23,24 @@ struct GeslachtView : View {
                 value.wrappedValue = .male
             } label: {
                 VStack{
-                    Image(systemName: value.wrappedValue == .male ? "circle.fill" : "circle")
+                    if value.wrappedValue == .male {
+                        Image(systemName: "person.circle.fill")
+                            .font(.system(size: 50))
+                            .padding(30)
+                            .foregroundColor(.white)
+                            .background(Color("BoyBlue"))
+                            .cornerRadius(20)
+                    } else {
+                        Image(systemName: "person.circle")
+                            .font(.system(size: 50))
+                            .padding(30)
+                            .foregroundColor(Color("AccentColor"))
+                            .background(Color("ColorWhite"))
+                            .cornerRadius(20)
+                    }
+                    //Image(systemName: value.wrappedValue == .male ? "circle.fill" : "circle")
                     Text("Man")
+                        .foregroundColor(Color("TextColor"))
                 }
             }
 
@@ -33,8 +49,24 @@ struct GeslachtView : View {
                 value.wrappedValue = .female
             } label: {
                 VStack{
-                    Image(systemName: value.wrappedValue == .female ? "circle.fill" : "circle")
+                    if  value.wrappedValue == .female{
+                        Image(systemName: "person.circle.fill")
+                            .font(.system(size: 50))
+                            .padding(30)
+                            .foregroundColor(.white)
+                            .background(Color("GirlPink"))
+                            .cornerRadius(20)
+                    } else {
+                        Image(systemName: "person.circle")
+                            .font(.system(size: 50))
+                            .padding(30)
+                            .foregroundColor(Color("AccentColor"))
+                            .background(Color("ColorWhite"))
+                            .cornerRadius(20)
+                    }
+                    //Image(systemName: value.wrappedValue == .female ? "circle.fill" : "circle")
                     Text("Vrouw")
+                        .foregroundColor(Color("TextColor"))
                 }
             }
         }
